b21eef24ae96ba188b0015384fa2e4cb56ed5f81
GalaxyWolfCloud <79249476+GalaxyWolfCloud@users.noreply.github.com>
2022-01-29 11:46:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+BTS_butter="";
+harry_potter="";
+
 function setup(){
     canvas = createCanvas(600,530);
     canvas.center();
@@ -5,4 +8,20 @@ function setup(){
     video = createCapture(VIDEO);
     video.hide();
 }
+
+function preload(){
+    BTS_butter = loadSound("music2.mp3");
+    harry_potter = loadSound("music.mp3");
+}
+
+function draw(){
+    image(video,0,0,600,530);
+}
+
+function play()
+{
+    BTS_butter.play();
+    
+}
+
 
