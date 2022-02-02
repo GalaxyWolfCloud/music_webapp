f636873ea78837f42eb7c83b9ecaf6bc27eb2400
GalaxyWolfCloud <79249476+GalaxyWolfCloud@users.noreply.github.com>
2022-02-02 18:30:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,57 @@
-BTS_butter="";
-harry_potter="";
+BTS_Butter ="";
+Harry_potter ="";
+rightWrist_x = 0;
+rightWrist_y = 0;
+leftWrist_x = 0;
+leftWrist_y = 0;
 
-function setup(){
+function setup()
+{
     canvas = createCanvas(600,530);
     canvas.center();
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotposes);
 }
 
-function preload(){
-    BTS_butter = loadSound("music2.mp3");
-    harry_potter = loadSound("music.mp3");
+function preload()
+{
+    BTS_Butter = loadSound("music2.mp3");
+    Harry_potter = loadSound("music.mp3");
 }
 
-function draw(){
+function draw()
+{
     image(video,0,0,600,530);
 }
-
 function play()
 {
-    BTS_butter.play();
-    
+    song.play(music.mp3);
+    song.setVolume(1);
+    song.rate(1);
 }
 
+function modelLoaded()
+{
+    console.log("poseNet Is Initialized");
+}
+
+function gotposes(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+         
+        leftWrist_x = results[0].pose.leftWrist.x;
+        leftWrist_y = results[0].pose.leftWrist.y;
+        console.log("leftWrist_x = "+leftWrist_x+" leftWrist_y = "+leftWrist_y);
+
+        rightWrist_x = results[0].pose.rightWrist.x;
+        rightWrist_y = results[0].pose.rightWrist.y;
+        console.log("rightWrist_x = "+rightWrist_x+" rightWrist_y = "+rightWrist_y);
+    }
+}
 
